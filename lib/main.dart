ed64515d6ba3909f79e58aa47dbe8d7d6f262282
shuheydev <shuhey.dev@gmail.com>
2020-07-25 14:43:58 +0900

    TextにStyleを設定した.

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,28 @@ void main() {
     home: Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.red[600],
-        title: Text('my first app'),
+        title: Text(
+          'my first app',
+        ),
         centerTitle: true,
       ),
       body: Center(
-        child: Text('hello ninjas!'),
+        child: Text(
+          'hello ninjas!',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.grey[600],
+            letterSpacing: 2.0,
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.red[600],
-        child: Text('Click'),
+        child: Text(
+          'Click',
+        ),
+        onPressed: () {},
       ),
     ),
   ));
