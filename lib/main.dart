d1b80b03e2ab739c6cb3dea515a81dc2efaf1b49
shuheydev <shuhey.dev@gmail.com>
2020-07-25 14:32:48 +0900

    Scaffoldを使ってシンプルなページを作成した.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Text('hey nijas!'),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('my first app'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text('hello ninjas!'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('Click'),
+      ),
+    ),
   ));
 }
