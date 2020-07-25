55276fe052dd5f226098a8f1c9ab8b3c34cc7e84
shuheydev <shuhey.dev@gmail.com>
2020-07-25 15:51:46 +0900

    プロジェクトに画像を追加してそれを表示させる
    pubspec.yamlに各画像のパスを追加した.

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,7 @@ class Home extends StatelessWidget {
       ),
       body: Center(
         child: Image(
-          image: NetworkImage(
-              'https://images.unsplash.com/photo-1505506874110-6a7a69069a08?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80'),
+          image: AssetImage('assets/space2.jpg'),
         ),
       ),
       floatingActionButton: FloatingActionButton(
