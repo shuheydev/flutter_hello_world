da50364eb0f6ef8633bcc370bd9ae4403ed7db85
shuheydev <shuhey.dev@gmail.com>
2020-07-25 15:22:42 +0900

    ページの内容をStatelessWidgetにした.

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red[600],
-        title: Text(
-          'my first app',
-        ),
-        centerTitle: true,
+void main() => runApp(
+      MaterialApp(
+        home: Home()
       ),
-      body: Center(
-        child: Text(
-          'はろー,忍者!',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.grey[600],
-            letterSpacing: 2.0,
-            fontFamily: 'NotoSerifJP',
+    );
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.red[600],
+            title: Text(
+              'my first app',
+            ),
+            centerTitle: true,
           ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.red[600],
-        child: Text(
-          'Click',
-        ),
-        onPressed: () {},
-      ),
-    ),
-  ));
+          body: Center(
+            child: Text(
+              'はろー,忍者!',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey[600],
+                letterSpacing: 2.0,
+                fontFamily: 'NotoSerifJP',
+              ),
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            backgroundColor: Colors.red[600],
+            child: Text(
+              'click',
+            ),
+            onPressed: () {},
+          ),
+        );
+  }
 }
