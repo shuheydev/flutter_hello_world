ac39104e7e661616675d249c0ede56e88f5b06e8
shuheydev <shuhey.dev@gmail.com>
2020-07-25 16:46:33 +0900

    ボタン色々.

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,52 @@ class Home extends StatelessWidget {
       body: Center(
         child: Column(
           children: <Widget>[
+            IconButton(
+              onPressed: (){
+                print('Icon button pressed');
+              },
+              hoverColor: Colors.redAccent,
+              icon:Icon(Icons.alternate_email),
+              color: Colors.amber,
+            ),
+            RaisedButton.icon(
+              onPressed: () {
+                print('you clicked me');
+              },
+              label: Text(
+                'メール送信',
+                style: TextStyle(
+                  fontFamily: 'NotoSerifJP',
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              icon: Icon(
+                Icons.mail,
+                color: Colors.white,
+              ),
+              color: Colors.lightBlue,
+              elevation: 10.0,
+            ),
+            FlatButton(
+              onLongPress: () {
+                print('you long pressed me');
+              },
+              child: Text(
+                'Logn press',
+                style: TextStyle(
+                  decoration: TextDecoration.combine(
+                    <TextDecoration>[
+                      TextDecoration.lineThrough,
+                      TextDecoration.overline,
+                      TextDecoration.underline,
+                    ],
+                  ),
+                ),
+              ),
+              color: Colors.lightGreen,
+              textColor: Colors.white,
+            ),
             Icon(
               Icons.airport_shuttle,
               color: Colors.lightBlue,
