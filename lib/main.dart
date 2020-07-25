62d90c2dd562784cbe6e4212d3c003bf0efe9a36
shuheydev <shuhey.dev@gmail.com>
2020-07-25 15:40:02 +0900

    Webサイトの画像を表示させる.NetworkImage.

--- lib/main.dart
+++ lib/main.dart
@@ -2,40 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
 void main() => runApp(
-      MaterialApp(
-        home: Home()
+      MaterialApp(home: Home()),
+    );
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red[600],
+        title: Text(
+          'my first app',
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Image(
+          image: NetworkImage(
+              'https://images.unsplash.com/photo-1505506874110-6a7a69069a08?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red[600],
+        child: Text(
+          'click',
+        ),
+        onPressed: () {},
       ),
     );
-class Home extends StatelessWidget{
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.red[600],
-            title: Text(
-              'my first app',
-            ),
-            centerTitle: true,
-          ),
-          body: Center(
-            child: Text(
-              'はろー,忍者!',
-              style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.grey[600],
-                letterSpacing: 2.0,
-                fontFamily: 'NotoSerifJP',
-              ),
-            ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            backgroundColor: Colors.red[600],
-            child: Text(
-              'click',
-            ),
-            onPressed: () {},
-          ),
-        );
   }
 }
